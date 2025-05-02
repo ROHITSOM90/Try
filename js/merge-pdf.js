a1246046e42eb95f5cabb61857dfb7d5d1bafd76
ROHITSOM90 <rohitsom90@gmail.com>
2025-05-02 12:49:31 +0530

    Create merge-pdf.js

---
+++ js/merge-pdf.js
@@ -0,0 +1,3 @@
+<!-- Add to the <head> section of each tool page -->
+<script src="https://cdn.jsdelivr.net/npm/pdf-lib@1.17.1/dist/pdf-lib.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js"></script>
